c6f7e0303bb0178bebded6b20107bb0ec547e91e
aaryan-sood <soodaaryan7@gmail.com>
2024-08-04 17:58:30 +0530

    first commit

---
+++ rangeSumOfSortedSubaaraySum.cpp
@@ -0,0 +1,9 @@
+//problem link
+// https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/description/
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"aaryan"<<endl;
+    return 0;
+}
