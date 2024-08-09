f79d0bdebb47a11a34600580720e1676223ac19f
aaryan-sood <soodaaryan7@gmail.com>
2024-08-09 23:36:22 +0530

    added two pointer approach

--- 977SqauresofSortedArray.cpp
+++ 977SqauresofSortedArray.cpp
@@ -12,3 +12,24 @@ public:
         return ans;
     }
 };
+//Alternate Solution
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int left=0,right=nums.size() - 1;
+        vector<int> ans;
+        while(left <= right)
+        {
+            if(abs(nums[left]) > abs(nums[right]))
+            {
+                ans.push_back(pow(nums[left++] , 2));
+            }
+            else
+            {
+                ans.push_back(pow(nums[right--] , 2));
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
