8c8e9cc5591638e34287298ddefae89f3495afa3
aaryan-sood <soodaaryan7@gmail.com>
2024-08-09 16:41:26 +0530

    added code

--- 206reverselinkedList.cpp
+++ 206reverselinkedList.cpp
@@ -33,3 +33,37 @@ public:
 
     }
 };
+
+// another solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        vector<int> ans;
+        while(head != NULL)
+        {
+            ans.push_back(head->val);
+            head=head->next;
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.empty())
+        return NULL;
+        ListNode * new_head=new ListNode(ans[0]);
+        ListNode * curr=new_head;
+        for(int i=1;i<ans.size();i++)
+        {
+            curr->next=new ListNode(ans[i]);
+            curr=curr->next;
+        }
+        return new_head;
+    }
+};
