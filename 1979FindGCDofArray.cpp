05a1e1fe6f78ce5c7ad980f5e0b98bb5d0f3d2ab
aaryan-sood <soodaaryan7@gmail.com>
2024-09-01 17:49:23 +0530

    add code

---
+++ 1979FindGCDofArray.cpp
@@ -0,0 +1,24 @@
+// problem link
+// https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+class Solution {
+public:
+
+    int helper(int a,int b)
+    {
+        if(a == 0)
+        {
+            return b;
+        }
+        else
+        {
+            return helper(b%a,a);
+        }
+    }
+    int findGCD(vector<int>& nums) {
+        int a=*min_element(nums.begin(),nums.end());
+
+        int b=*max_element(nums.begin(),nums.end());
+
+        return helper(a,b);
+    }
+};
