9086b03a0b12a3cdb1c3c48dbbcb2aeec3458193
aaryan-sood <soodaaryan7@gmail.com>
2024-12-25 19:52:17 +0530

    added code

--- 120Triangle.cpp
+++ 120Triangle.cpp
@@ -1,16 +1,14 @@
 // problem link
 // https://leetcode.com/problems/triangle/description/
+// top down approach
 class Solution {
 public:
-    int helper(vector<vector<int>> &triangle,vector<vector<int>> &dp,int i,int j,int n,int m)
+    int helper(vector<vector<int>> &triangle,vector<vector<int>> &dp,int i,int j,int n)
     {
-        if(i == n && j <= m)
+        if(i == n)
         {
-            return dp[i][j]=triangle[i][j];
-        }
-        else if(i >n || j > m)
-        {
-            return 1e9;
+            dp[n][j]=triangle[n][j];
+            return dp[n][j];
         }
         else if(dp[i][j] != -1)
         {
@@ -18,13 +16,40 @@ public:
         }
         else
         {
-            return dp[i][j]=min(triangle[i][j] + helper(triangle,dp,i+1,j,n,m),triangle[i][j] + helper(triangle,dp,i+1,j+1,n,m));
+            int bottom=triangle[i][j] + helper(triangle,dp,i+1,j,n);
+            int bottomRight=triangle[i][j] + helper(triangle,dp,i+1,j+1,n);
+            dp[i][j]=min(bottom,bottomRight);
+            return dp[i][j];
         }
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[n-1].size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return helper(triangle,dp,0,0,n-1,m-1);
+        return helper(triangle,dp,0,0,n-1);
+    }
+};
+// bottom up approach
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        int m=triangle[n-1].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                if(i == n-1)
+                {
+                    dp[i][j] = triangle[i][j];
+                }
+                else
+                {
+                    dp[i][j] = min(triangle[i][j] + dp[i+1][j],triangle[i][j] + dp[i+1][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
