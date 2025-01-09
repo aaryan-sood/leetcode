60f1a2f6c9c6570d9c189985a6be8df189c52405
aaryan-sood <soodaaryan7@gmail.com>
2025-01-09 11:59:13 +0530

    added tabulation solution

--- gfg/perfectSumProblem.cpp
+++ gfg/perfectSumProblem.cpp
@@ -20,25 +20,71 @@ class Solution {
     }
 
     int perfectSum(vector<int>& arr, int target) {
-        int n = arr.size();
-        if (n == 0) return 0;
+        int n=arr.size();
+        
+        if(n == 0) return 0;
         
         int zeroCount = 0;
-        for (int x : arr) {
-            if (x == 0) zeroCount++;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] == 0)
+            {
+                zeroCount++;
+            }
         }
         
-        vector<int> filteredArr;
-        for (int x : arr) {
-            if (x != 0) filteredArr.push_back(x);
-        }
-
-        int m = filteredArr.size();
-        vector<vector<int>> dp(target + 1, vector<int>(m, -1));
+        vector<int> filterArr;
         
-        int result = helper(filteredArr, dp, target, m - 1);
-
-        // Multiply result by 2^zeroCount to account for all combinations of zeros
-        return result * (1 << zeroCount);
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] != 0)
+            {
+                filterArr.push_back(arr[i]);
+            }
+        }
+        
+        int m=filterArr.size();
+        if(m == 0)
+        {
+            return (1 << zeroCount);
+        }
+        vector<vector<int>> dp(target + 1,vector<int>(m,-1));
+        
+        // number of subsets which sum to 0
+        for(int i = 0;i<dp[0].size();i++)
+        {
+            dp[0][i] = 1;
+        }
+        
+        // number of subsets which sum to 0 to target with first element
+        for(int i=1;i<dp.size();i++)
+        {
+            if(filterArr[0] == i)
+            {
+                dp[i][0] = 1;
+            }
+            else
+            {
+                dp[i][0] = 0;
+            }
+        }
+        
+        for(int i=1;i<dp.size();i++)
+        {
+            for(int j=1;j<dp[0].size();j++)
+            {
+                int exclude = dp[i][j-1];
+                int include = 0;
+                if(i - filterArr[j] >= 0)
+                {
+                    include = dp[i - filterArr[j]][j-1];
+                }
+                dp[i][j] = include + exclude;
+                
+            }
+        }
+        
+        
+        return dp[target][m-1] * (1 << zeroCount);
     }
 };
