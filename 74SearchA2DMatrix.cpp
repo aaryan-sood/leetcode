c9e8922b01cf18fccde6fefbbd56263d727e9195
Aaryan Sood <soodaaryan7@gmail.com>
2025-03-18 19:55:18 +0530

    added code

--- 74SearchA2DMatrix.cpp
+++ 74SearchA2DMatrix.cpp
@@ -1,27 +1,27 @@
 // problem link
 // https://leetcode.com/problems/search-a-2d-matrix/description/
 class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows=matrix.size();
-        int columns=matrix[0].size();
-        int start=0,end=rows*columns - 1;
-        while(start <= end)
-        {
-            int mid=start+(end-start)/2;
-            if(target == matrix[mid/columns][mid%columns])
+    public:
+        bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            int n = matrix.size(),m = matrix[0].size();
+            int start = 0,end = n * m - 1;
+            while(start <= end)
             {
-                return true;
+                int mid = (end - start)/2 + start;
+                int row = mid/m,col = mid%m;
+                if(matrix[row][col] == target)
+                {
+                    return true;
+                }
+                else if(target < matrix[row][col])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
             }
-            else if(matrix[mid/columns][mid%columns] < target)
-            {
-                start=mid+1;
-            }
-            else
-            {
-                end=mid-1;
-            }
+            return false;
         }
-        return false;
-    }
-};
+    };
