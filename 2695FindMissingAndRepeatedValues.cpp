0b89b1c08e6981635ba225e717413760fe8e61eb
Aaryan <97805271+aaryan-sood@users.noreply.github.com>
2026-08-30 23:52:58 +0530

    added code

---
+++ 2695FindMissingAndRepeatedValues.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> nums(n * n + 1, 0);
+
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < n;j++){
+                nums[grid[i][j]]+=1;
+            }
+        }
+        vector<int> ans;
+        for(int i = 1;i < n*n + 1;i++){
+            if(nums[i] == 2){
+                ans.insert(ans.begin(),i);
+            }
+            else if(nums[i] == 0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
