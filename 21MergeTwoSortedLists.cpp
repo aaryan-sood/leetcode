86543eec0068367853063e2d2fac6832235ce730
Aaryan <97805271+aaryan-sood@users.noreply.github.com>
2026-09-09 01:17:16 +0530

    added code

--- 21MergeTwoSortedLists.cpp
+++ 21MergeTwoSortedLists.cpp
@@ -61,3 +61,85 @@ public:
         }
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        ListNode *head = NULL, *ptr = NULL;
+        while (list1 != NULL && list2 != NULL){
+            if (list1->val <= list2->val){
+                ListNode *temp = new ListNode(list1->val);
+                if (ptr == NULL){
+                    ptr = temp;
+                }
+                else{
+                    ptr->next = temp;
+                    ptr = ptr->next;
+                }
+                list1 = list1->next;
+            }
+            else{
+                ListNode *temp = new ListNode(list2->val);
+                if (ptr == NULL){
+                    ptr = temp;
+                }
+                else{
+                    ptr->next = temp;
+                    ptr = ptr->next;
+                }
+                list2 = list2->next;
+            }
+
+            if (head == NULL){
+                head = ptr;
+            }
+        }
+
+        while (list1 != NULL){
+            ListNode *temp = new ListNode(list1->val);
+            if (ptr == NULL){
+                ptr = temp;
+            }
+            else{
+                ptr->next = temp;
+                ptr = ptr->next;
+            }
+            list1 = list1->next;
+
+            if (head == NULL){
+                head = ptr;
+            }
+        }
+
+        while (list2 != NULL){
+
+            ListNode *temp = new ListNode(list2->val);
+            if (ptr == NULL){
+                ptr = temp;
+            }
+            else{
+                ptr->next = temp;
+                ptr = ptr->next;
+            }
+            list2 = list2->next;
+
+            if (head == NULL){
+                head = ptr;
+            }
+        }
+        return head;
+    }
+};
+
