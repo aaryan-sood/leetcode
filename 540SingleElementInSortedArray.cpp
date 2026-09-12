6f659473f02517fc25fcc24f2e561ff4e811ad84
Aaryan <97805271+aaryan-sood@users.noreply.github.com>
2026-09-12 16:59:54 +0530

    added code

---
+++ 540SingleElementInSortedArray.cpp
@@ -0,0 +1,11 @@
+// problem link: https://leetcode.com/problems/single-element-in-a-sorted-array/
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0;i < nums.size();i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
